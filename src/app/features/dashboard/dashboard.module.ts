a960582729175889397aaeacc806add98b2d61d1
Carlos Abramonte <car.abr.04@gmail.com>
2026-07-14 03:01:33 -0500

    Se agrego un popover de confirmacion para cerrar session

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -4,9 +4,15 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 
 @NgModule({
   declarations: [DashboardComponent, HeaderComponent],
-  imports: [CommonModule, DashboardRoutingModule, NzAvatarModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    NzAvatarModule,
+    NzPopconfirmModule,
+  ],
 })
 export class DashboardModule {}
