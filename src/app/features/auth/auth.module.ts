c3ec4c61da6483d1c2886cd5a1bb7cf413c0c653
Carlos Abramonte <car.abr.04@gmail.com>
2026-07-14 02:37:37 -0500

    Se eliminaron componentes y servicios innecesarios

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-import { RecoveryPassComponent } from './components/recovery-pass/recovery-pass.component';
-import { LoginComponent } from './components/login/login.component';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent, RecoveryPassComponent],
+  declarations: [AuthComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
